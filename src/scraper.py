5033d36539ea24e78ae2f68d1dd32220b8aead51
Mantas Pieza <pieza.mantas@gmaile.com>
2023-03-13 22:07:11 +0200

    MVP scrapper finished and tested

---
+++ src/scraper.py
@@ -0,0 +1,78 @@
+import os
+import time
+import requests
+import numpy as np
+import pandas as pd
+from bs4 import BeautifulSoup, ResultSet
+
+
+class Scraper:
+    def __init__(
+        self,
+        id: str = "CodeAcademy-Final-Project",
+        web_browser: str = "Mozilla/5.0",
+        general_url_for_monthly_data: str = "https://www.nyc.gov/site/tlc/about/tlc-trip-record-data.page",
+        scraping_start_year_month: str = "2022-01",
+        scraping_end_year_month: str = "2022-01",
+        title_of_parquet_file: str = "scraped_yellow_city_taxi_file",
+        timeout: int = 2,
+    ):
+        self.header = {id: web_browser}
+        self.url_for_scraping = general_url_for_monthly_data
+        self.title_of_parquet_file = title_of_parquet_file
+        self.timeout = timeout
+        self.start_date = scraping_start_year_month
+        self.end_date = scraping_end_year_month
+        self._datasets_to_download = []
+
+    def get_page_response(self, url: str, header: dict) -> BeautifulSoup:
+        self.response = requests.get(url, headers=header)
+
+        soup = BeautifulSoup(self.response.text, "html.parser")
+        if not self.response.ok:
+            print(f"You've received {self.response} error")
+        else:
+            return soup
+
+    def collect_all_datasets_to_download(self) -> list:
+        soup = self.get_page_response(url=self.url_for_scraping, header=self.header)
+
+        all_downloadable_files = soup.find_all(
+            "a", title="Yellow Taxi Trip Records", href=True
+        )
+
+        return [dataset["href"] for dataset in all_downloadable_files]
+
+    def generate_dataset_names(self) -> list:
+        month_list = pd.period_range(start=self.start_date, end=self.end_date, freq="M")
+
+        return [month.strftime("%Y-%m.parquet") for month in month_list]
+
+    def identify_required_datasets(self) -> list:
+        all_possible_datasets_to_download = self.collect_all_datasets_to_download()
+        required_datasets_names = self.generate_dataset_names()
+
+        for item in all_possible_datasets_to_download:
+            for dataset_name in required_datasets_names:
+                if dataset_name in item.split("_"):
+                    self._datasets_to_download.append(item)
+
+    def download_required_datasets(self):
+        # retrieves all files in folder to avoid duplicate download
+        files_in_folder = os.listdir("../raw_data")
+        self.identify_required_datasets()
+
+        for dataset in self._datasets_to_download:
+            dataset_name = dataset.split("/")[-1]
+            try:
+                if dataset_name not in files_in_folder:
+                    downloaded_dataset = requests.get(dataset).content
+
+                    with open(f"../raw_data/{dataset_name}", "wb") as handler:
+                        handler.write(downloaded_dataset)
+                else:
+                    print("dataset alraedy exists")
+                    pass
+            except Exception as e:
+                print(e)
+            
