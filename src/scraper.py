0d8e50681189f13456487b1492eb148c403e949f
Mantas Pieza <pieza.mantas@gmail.com>
2023-03-25 13:15:22 +0200

    dockstrings created, formatted.

--- src/scraper.py
+++ src/scraper.py
@@ -7,6 +7,9 @@ from bs4 import BeautifulSoup, ResultSet
 
 
 class Scraper:
+    """
+    This is a scraper class which extracts monthly New York City Yellow taxi trip datasets from the source
+    """    
     def __init__(
         self,
         id: str = "CodeAcademy-Final-Project",
@@ -15,8 +18,20 @@ class Scraper:
         scraping_start_year: str = "2022-01",
         scraping_end_year: str = "2022-01",
         title_of_parquet_file: str = "scraped_yellow_city_taxi_file",
-        timeout: int = 2,
-    ):
+        timeout: int = 1,
+    ) -> None:
+        """ 
+        Initialisation function.
+
+        Args:
+            id (str, optional): Self indentification used in BeautifulSoup. Defaults to "CodeAcademy-Final-Project".
+            web_browser (str, optional): selection of web browser used in BeautifulSoup. Defaults to "Mozilla/5.0".
+            general_url_for_monthly_data (_type_, optional): Page URL to retrieve data. Defaults to "https://www.nyc.gov/site/tlc/about/tlc-trip-record-data.page".
+            scraping_start_year (str, optional): Start year to extract data from. Defaults to "2022-01".
+            scraping_end_year (str, optional): End year to extract data to. Defaults to "2022-01".
+            title_of_parquet_file (str, optional): start file name to save retrieve data to. Defaults to "scraped_yellow_city_taxi_file".
+            timeout (int, optional): Number of seconds between each scraping action. Defaults to 1.
+        """        
         self.header = {id: web_browser}
         self.url_for_scraping = general_url_for_monthly_data
         self.title_of_parquet_file = title_of_parquet_file
@@ -26,6 +41,16 @@ class Scraper:
         self._datasets_to_download = []
 
     def get_page_response(self, url: str, header: dict) -> BeautifulSoup:
+        """
+        Checks if page is responsive and gets the connection.
+        
+        Args:
+            url (str): Page URL to retrieve data
+            header (dict): Combination of ID and web-berowser
+
+        Returns:
+            BeautifulSoup object
+        """        
         self.response = requests.get(url, headers=header)
 
         soup = BeautifulSoup(self.response.text, "html.parser")
@@ -35,6 +60,12 @@ class Scraper:
             return soup
 
     def collect_all_datasets_to_download(self) -> list:
+        """
+        Extracts all datasets names from scraped web page html
+
+        Returns:
+            list: list of all dataset names possible to download.
+        """
         soup = self.get_page_response(url=self.url_for_scraping, header=self.header)
 
         all_downloadable_files = soup.find_all(
@@ -44,11 +75,23 @@ class Scraper:
         return [dataset["href"] for dataset in all_downloadable_files]
 
     def generate_dataset_names(self) -> list:
+        """
+        Creates a list of required dataset names using start and end date from init function
+
+        Returns:
+            list: List of required datasets to download.
+        """
         month_list = pd.period_range(start=self.start_date, end=self.end_date, freq="M")
         
         return [month.strftime("%Y-%m.parquet") for month in month_list if month.strftime("%m") == '12' ]
 
     def identify_required_datasets(self) -> list:
+        """
+        Generates a list of datasets to download, if generated dataset name is in dataset list possible to download to.
+
+        Returns:
+            list: Final list of datasets to download.
+        """
         all_possible_datasets_to_download = self.collect_all_datasets_to_download()
         required_datasets_names = self.generate_dataset_names()
 
@@ -59,7 +102,13 @@ class Scraper:
                 else:
                     pass
 
-    def download_required_datasets(self, path_to_raw_dataset_folder:str ='../raw_data'):
+    def download_required_datasets(self, path_to_raw_dataset_folder:str ='../raw_data') -> None:
+        """
+        Downloads required dataset to raw data folder, checking if the file does exist.
+
+        Args:
+            path_to_raw_dataset_folder (str, optional): Path to save dowloaded datasets. Defaults to '../raw_data'.
+        """
         # retrieves all files in folder to avoid duplicate download
         files_in_folder = os.listdir(path_to_raw_dataset_folder)
         self.identify_required_datasets()
@@ -68,6 +117,7 @@ class Scraper:
             dataset_name = dataset.split("/")[-1]
             try:
                 if dataset_name not in files_in_folder:
+                    time.sleep(self.timeout)
                     downloaded_dataset = requests.get(dataset).content
 
                     with open(f"{path_to_raw_dataset_folder}/{dataset_name}", "wb") as handler:
--- src/transformer.py
+++ src/transformer.py
@@ -9,6 +9,10 @@ pd.options.display.float_format = "{:.3f}".format
 
 
 class Transformer:
+    """
+    Transformer class which takes in raw dataset, removes outliers and non required columns providing transformed dataset.
+    """
+
     def __init__(
         self,
         raw_file_location: str = "../raw_data/",
@@ -17,20 +21,30 @@ class Transformer:
         file_name: str = "yellow_tripdata_2021-12.parquet",
         transofmed_data_path="../transformed_data/",
     ):
-        # self._raw_file_location = raw_file_location
+        """
+        Initialization function for Transformer class.
+
+        Args:
+            raw_file_location (str, optional): Location where raw datasets are stored. Defaults to "../raw_data/".
+            period_start_day (str, optional): Start date to define date for dataset filtering. Defaults to 24.
+            period_end_day (str, optional): End date to define date for dataset filtering. Defaults to 26.
+            file_name (str, optional): File name used to identify dataset. Defaults to "yellow_tripdata_2021-12.parquet".
+            transofmed_data_path (str, optional): System path where to save transformed datasets. Defaults to "../transformed_data/".
+        """
+
         self.period_start_day = period_start_day
         self.period_end_day = period_end_day
         self.file_name = file_name
         self.dataframe = pd.read_parquet(f"{raw_file_location}{file_name}")
         self.transformed_data_path = transofmed_data_path
 
-        # init metu paduot pavadinima .parquet failo, ir init metu pd.read_parquet
+    def get_start_end_datetime(self, dataframe_name: str) -> None:
+        """
+        Identifies Start and end dates for Pickup and dropoff datetime columns according to predefined rules and saves them as class variables.
 
-    # def read_dataframe(self, dataframe_name: str) -> pd.DataFrame:
-    #     df = pd.read_parquet(f"{self._raw_file_location}{dataframe_name}")
-    #     return df
-
-    def get_start_end_datetime(self, dataframe_name: str):
+        Args:
+            dataframe_name (str): File name used to identify dataset.
+        """
         year_month = dataframe_name.split("_")[-1].split(".")[0]
 
         self.start_date = dt.strptime(year_month, "%Y-%m") + relativedelta(
@@ -39,7 +53,11 @@ class Transformer:
         self.end_date = self.start_date + relativedelta(days=3)
         self.end_date_dropoff = self.start_date + relativedelta(days=3, hours=1)
 
-    def select_christmas_period(self) -> pd.DataFrame:
+    def select_christmas_period(self) -> None:
+        """
+        Performs daframe filtering using defined start and end dates for Pickup and dropoff datetime columns.
+        Overwrites dataset defined in class initiation.
+        """
         self.get_start_end_datetime(dataframe_name=self.file_name)
 
         self.dataframe = self.dataframe[
@@ -52,12 +70,20 @@ class Transformer:
             & (self.dataframe["tpep_dropoff_datetime"] < self.end_date_dropoff)
         ]
 
-    def restrict_vendor_id(self) -> pd.DataFrame:
+    def restrict_vendor_id(self) -> None:
+        """
+        Removes outliers from VendorID column.
+        Overwrites dataset defined in class initiation.
+        """
         self.dataframe = self.dataframe[
             (self.dataframe["VendorID"] == 1) | (self.dataframe["VendorID"] == 2)
         ]
 
-    def create_trip_duration_column(self) -> pd.DataFrame:
+    def create_trip_duration_column(self) -> None:
+        """
+        Creates trip duration column, which is used as a target variable in model creation.
+        Overwrites dataset defined in class initiation.
+        """
         self.dataframe["trip_duration_minutes"] = (
             (
                 self.dataframe["tpep_dropoff_datetime"]
@@ -65,9 +91,15 @@ class Transformer:
             ).dt.total_seconds()
         ) / 60
 
-    def restrict_passenger_count(
-        self, maximum_number_of_passengers: int = 6
-    ) -> pd.DataFrame:
+    def restrict_passenger_count(self, maximum_number_of_passengers: int = 6) -> None:
+        """
+        Removes outliers from passenger count column.
+        Overwrites dataset defined in class initiation.
+
+        Args:
+            maximum_number_of_passengers (int, optional): Number of maximum passengers in taxi. Defaults to 6.
+
+        """
         passenger_count_mode_value = self.dataframe["passenger_count"].mode()
         self.dataframe["passenger_count"].fillna(
             passenger_count_mode_value, inplace=True
@@ -78,7 +110,16 @@ class Transformer:
 
     def restrict_ratecodeid(
         self, ratecode_1: int = 1, ratecode_2: int = 2, ratecode_3: int = 3
-    ):
+    ) -> None:
+        """
+        Fills in missing values using most freaquent (mode) value of the Rate Code ID column and removes outliers.
+        Overwrites dataset defined in class initiation.
+
+        Args:
+            ratecode_1 (int, optional): First ratecodeid. Defaults to 1.
+            ratecode_2 (int, optional): Second ratecodeid. Defaults to 2.
+            ratecode_3 (int, optional): Third ratecodeid. Defaults to 3.
+        """
         ratecode_id_mode = self.dataframe["RatecodeID"].mode()
         self.dataframe["RatecodeID"].fillna(ratecode_id_mode, inplace=True)
 
@@ -88,42 +129,38 @@ class Transformer:
             | (self.dataframe["RatecodeID"] == ratecode_3)
         ]
 
-    def restrict_store_fwd_flag(self):
+    def restrict_store_fwd_flag(self) -> None:
+        """
+        Fills in missing values in store and forward flag column, assuming with yes (Y) value.
+        Overwrites dataset defined in class initiation.
+        """
         self.dataframe["store_and_fwd_flag"].fillna("Y", inplace=True)
 
-    def restrict_payment_type(self):
+    def restrict_payment_type(self) -> None:
+        """
+        Restricts payment type to cash (2) or credit_card (1).
+        Overwrites dataset defined in class initiation.
+        """
         self.dataframe = self.dataframe[
             (self.dataframe["payment_type"] == 1)
             | (self.dataframe["payment_type"] == 2)
         ]
 
-    # def transform_congestion_surcharge(self):
-    #     if self.file_name != 'yellow_tripdata_2018-12.parquet':
-    #         self.dataframe = self.dataframe[
-    #             (self.dataframe["congestion_surcharge"] == 0)
-    #             | (self.dataframe["congestion_surcharge"] == 2.5)
-    #         ]
-    #         self.dataframe["congestion_surcharge"] = self.dataframe[
-    #             "congestion_surcharge"
-    #         ].replace(2.5, 1)
-    #     else:
-    #         self.dataframe['congestion_surcharge'].fillna(0,inplace=True)
-
-    # def transform_airport_fee(self):
-    #     self.dataframe["airport_fee"].fillna(0, inplace=True)
-    #     self.dataframe = self.dataframe[
-    #         (self.dataframe["airport_fee"] == 0)
-    #         | (self.dataframe["airport_fee"] == 1.25)
-    # ]
-    # self.dataframe["airport_fee"] = self.dataframe["airport_fee"].replace(1.25, 1)
-
-    def filter_pickup_locations(self):
+    def filter_pickup_locations(self) -> None:
+        """
+        Filters out unknown pickup locations.
+        Overwrites dataset defined in class initiation.
+        """
         self.dataframe = self.dataframe[
             (self.dataframe["PULocationID"] != 264)
             | (self.dataframe["PULocationID"] != 265)
         ]
 
-    def filter_pickup_location_outliers(self):
+    def filter_pickup_location_outliers(self) -> None:
+        """
+        Filters out ouliers.
+        Overwrites dataset defined in class initiation.
+        """
         temp_pickup = (
             self.dataframe["PULocationID"].value_counts().reset_index(name="count")
         )
@@ -135,7 +172,11 @@ class Transformer:
             self.dataframe = self.dataframe[self.dataframe["PULocationID"] != location]
             self.dataframe = self.dataframe[self.dataframe["DOLocationID"] != location]
 
-    def filter_dropoff_location_outliers(self):
+    def filter_dropoff_location_outliers(self) -> None:
+        """
+        Filters out outliers.
+        Overwrites dataset defined in class initiation.
+        """
         temp_dropoff = (
             self.dataframe["DOLocationID"].value_counts().reset_index(name="count")
         )
@@ -147,13 +188,21 @@ class Transformer:
             self.dataframe = self.dataframe[self.dataframe["PULocationID"] != location]
             self.dataframe = self.dataframe[self.dataframe["DOLocationID"] != location]
 
-    def filter_dropoff_locations(self):
+    def filter_dropoff_locations(self)-> None:
+        """
+        Filters out unknown drop off locations.
+        Overwrites dataset defined in class initiation.
+        """
         self.dataframe = self.dataframe[
             (self.dataframe["DOLocationID"] != 264)
             | (self.dataframe["DOLocationID"] != 265)
         ]
 
-    def filter_trip_duration_outliers(self):
+    def filter_trip_duration_outliers(self)-> None:
+        """
+        Filters out trip duration outliers that are less or equal to 0, and greateror or equal than 99.5 percentile.
+        Overwrites dataset defined in class initiation.
+        """
         self.dataframe = self.dataframe[
             (
                 self.dataframe["trip_duration_minutes"]
@@ -162,7 +211,11 @@ class Transformer:
             & (self.dataframe["trip_duration_minutes"] > 0)
         ]
 
-    def filter_trip_distance_outliers(self):
+    def filter_trip_distance_outliers(self) -> None:
+        """
+        Filtesr out trip distance outliers that are less or equal to 0, and greater  than 99.5 percentile.
+        Overwrites dataset defined in class initiation.
+        """
         self.dataframe = self.dataframe[
             (
                 self.dataframe["trip_distance"]
@@ -171,7 +224,11 @@ class Transformer:
             & (self.dataframe["trip_distance"] > 0)
         ]
 
-    def filter_fare_amount_outliers(self):
+    def filter_fare_amount_outliers(self) -> None:
+        """
+        Filters out fare amount outliers from dataset.
+        Overwrites dataset defined in class initiation.
+        """
         self.dataframe = self.dataframe[
             (self.dataframe["fare_amount"] > 0)
             & (
@@ -180,13 +237,21 @@ class Transformer:
             )
         ]
 
-    def filter_tip_amount_outliers(self):
+    def filter_tip_amount_outliers(self)-> None:
+        """
+        Filters out tip amount outliers.
+        Overwrites dataset defined in class initiation.
+        """
         self.dataframe = self.dataframe[
             self.dataframe["tip_amount"]
             <= np.quantile(self.dataframe["tip_amount"], 0.999)
         ]
 
-    def filter_tolls_amount_outliers(self):
+    def filter_tolls_amount_outliers(self)-> None:
+        """
+        Filters out tolls amount outliers.
+        Overwrites dataset defined in class initiation.
+        """
         self.dataframe = self.dataframe[
             self.dataframe["tolls_amount"]
             <= np.quantile(self.dataframe["tolls_amount"], 0.99)
@@ -206,15 +271,30 @@ class Transformer:
             "airport_fee",
             "congestion_surcharge",
         ],
-    ):
+    ) -> None:
+        """
+        Removes outlier columns from dataset.
+        Overwrites dataset defined in class initiation.
+
+        Args:
+            columns_list (list, optional): List of columns to be removed from dataset. Defaults to [ "fare_amount", "tip_amount", "total_amount", "extra", "mta_tax", "improvement_surcharge", "airport_fee", "congestion_surcharge", ].
+        """
         self.dataframe.drop(columns_list, axis=1, inplace=True)
 
-    def create_weekend_column(self):
+    def create_weekend_column(self) -> None:
+        """
+        Creates boolean weekend feature identifying if the pickup takes place on weekend.
+        Overwrites dataset defined in class initiation.
+        """
         self.dataframe["is_weekend"] = (
             self.dataframe["tpep_pickup_datetime"].dt.weekday >= 5
         )
 
-    def create_weekday_column(self):
+    def create_weekday_column(self) -> None:
+        """
+        Creates weekday column identifying day of the week of the pickup date.
+        Overwrites dataset defined in class initiation.
+        """
         self.dataframe["weekday"] = self.dataframe["tpep_pickup_datetime"].dt.weekday
         self.dataframe["weekday"] = self.dataframe["weekday"].map(
             {
@@ -228,13 +308,31 @@ class Transformer:
             }
         )
 
-    def define_if_business_hours(self, value):
+    def define_if_business_hours(self, value:int) -> bool:
+        """
+        Defines if pickup time is within business hours.
+
+        Args:
+            value (int): Hour of the pickup time
+
+        Returns:
+            bool: True if in within business hours
+        """
         if value >= 7 and value <= 18:
             return True
         else:
             return False
 
-    def define_time_of_day(self, hour):
+    def define_time_of_day(self, hour:int) -> str:
+        """
+        Identifies time of the day.
+
+        Args:
+            hour (int): Hour of the pickup
+
+        Returns:
+            str: Time of day: Morning, Afternoon, Evening, Night
+        """
         if hour >= 5 and hour < 12:
             return "Morning"
         if hour >= 12 and hour < 17:
@@ -244,20 +342,37 @@ class Transformer:
         if 0 <= hour < 5 or 21 <= hour < 25:
             return "Night"
 
-    def create_hour_column(self):
+    def create_hour_column(self) -> None:
+        """
+        Extracts hour from pickup datetime column and saves it as new column.
+        Overwrites dataset defined in class initiation.
+        """
         self.dataframe["hour"] = self.dataframe["tpep_pickup_datetime"].dt.hour
 
-    def create_business_hour_column(self):
+    def create_business_hour_column(self) -> None:
+        """
+        Creates new column identifying if pickup took place within business hours.
+        Overwrites dataset defined in class initiation.
+        """
+
         self.dataframe["is_business_hours"] = self.dataframe["hour"].apply(
             self.define_if_business_hours
         )
 
-    def create_time_of_day_column(self):
+    def create_time_of_day_column(self) -> None:
+        """
+        Creates time of day column using "define_time_of_day" function.
+        Overwrites dataset defined in class initiation.
+        """
         self.dataframe["time_of_day"] = self.dataframe["hour"].apply(
             self.define_time_of_day
         )
 
-    def add_year_column(self):
+    def add_year_column(self) -> None:
+        """
+        Extracts year from pickup datetime column as new column.
+        Overwrites dataset defined in class initiation.
+        """
         self.dataframe["year"] = self.dataframe["tpep_pickup_datetime"].dt.year
 
     def remove_time_columns(
@@ -267,23 +382,29 @@ class Transformer:
             "tpep_dropoff_datetime",
             "hour",
         ],
-    ):
+    ) -> None:
+        """
+        Removes date-time related columns from dataset.
+
+        Args:
+            cols_to_remove (list, optional): List of columns which were used in date-time feature engineering. Defaults to [ "tpep_pickup_datetime", "tpep_dropoff_datetime", "hour", ].
+        """
         self.dataframe.drop(cols_to_remove, axis=1, inplace=True)
 
-    def prepare_dataset(self) -> pd.DataFrame:
-        # self.get_start_end_datetime(dataframe_name=self.file_name)
+    def prepare_dataset(self) -> None:
+        """
+        Function used to orchestrate all the transormation steps.
+        """
 
-        self.select_christmas_period()  ####
+        self.select_christmas_period()  
 
         # categorical columns
-        self.restrict_vendor_id()  #####
+        self.restrict_vendor_id()  
         self.create_trip_duration_column()
         self.restrict_passenger_count()
         self.restrict_ratecodeid()
         self.restrict_store_fwd_flag()
         self.restrict_payment_type()
-        # self.transform_congestion_surcharge()
-        # self.transform_airport_fee()
         self.filter_pickup_locations()
         self.filter_pickup_location_outliers()
         self.filter_dropoff_location_outliers()
@@ -295,7 +416,6 @@ class Transformer:
         self.filter_fare_amount_outliers()
         self.filter_tip_amount_outliers()
         self.filter_tolls_amount_outliers()
-        ### should i add total amount oulier filtering ?
 
         # removing outlier columns
         self.remove_outlier_columns()
@@ -312,6 +432,10 @@ class Transformer:
         self.remove_time_columns()
 
     def transform_data(self) -> None:
+        """
+        Main function used to read in dataset, perfrom transformations and save the transformed dataframe.
+        
+        """
         self.prepare_dataset()
 
         self.dataframe.to_parquet(
